c4746e9f05af51d5eab363b24bbd2c8ea5e53653
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2024-09-19 12:49:28 -0400

    rename site

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import { type MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Code4Community" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };

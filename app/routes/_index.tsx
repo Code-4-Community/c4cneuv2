3408824a7b37d095c48661352df9ab9e95f5e5a6
Griffin Michalak <81431781+GriffinMichalak@users.noreply.github.com>
2024-10-04 15:57:42 -0400

    ui changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
   const image = 'https://images.unsplash.com/photo-1499796683658-b659bc751db1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
   return (
-    <div className="main">
+    <div className="m-52">
       <MissionStatement
         weLove={weLove}
         subtitle="Learn more about CodeCommunity's mission below."

087345ebddea1e7eccd31d190f387e3452001467
Jane Kamata <janekamata8@gmail.com>
2024-10-05 16:28:50 -0400

    reusing projectsprops

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -1,35 +1,41 @@
-interface ProjectProps {
-    image?: string;
-    title?: string;
-    subtitle?: string;
-    description?: string;
-    link?: string;
+export interface ProjectProps {
+  image?: string;
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  link?: string;
 }
 
 const Project = ({
-    image,
-    title,
-    subtitle,
-    description,
-    link
+  image,
+  title,
+  subtitle,
+  description,
+  link,
 }: ProjectProps) => {
-    return (
-        <div className="flex flex-col items-center">
-            <img
-                src={image ?? 'https://i.imgur.com/BlBH8Ak.jpeg'}
-                alt={title}
-                className="w-48 h-48 object-cover rounded-lg shadow-lg mb-4"
-            />
-            <p className="text-lg font-semibold">{title ?? "Project Title"}</p>
-            <p className="text-sm text-gray-500 mb-4">{subtitle ?? "Project Subtitle"}</p>
-            <p className="text-sm text-gray-500 ml-4 mb-4">{description ?? "Project Description"}</p>
-            <div className="flex space-x-4">
-                {link && (<a href={link} >
-                    <button>View Case Study</button>
-                </a>)}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center">
+      <img
+        src={image ?? "https://i.imgur.com/BlBH8Ak.jpeg"}
+        alt={title}
+        className="w-48 h-48 object-cover rounded-lg shadow-lg mb-4"
+      />
+      <p className="text-lg font-semibold">{title ?? "Project Title"}</p>
+      <p className="text-sm text-gray-500 mb-4">
+        {subtitle ?? "Project Subtitle"}
+      </p>
+      <p className="text-sm text-gray-500 ml-4 mb-4">
+        {description ?? "Project Description"}
+      </p>
+      <div className="flex space-x-4">
+        {link && (
+          <a href={link}>
+            <button>View Case Study</button>
+          </a>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- app/components/projects-carousel.tsx
+++ app/components/projects-carousel.tsx
@@ -1,38 +1,40 @@
 import {
-    Carousel,
-    CarouselContent,
-    CarouselDots,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "../components/ui/carousel"
-import Project from "./project"
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../components/ui/carousel";
+import Project, { ProjectProps } from "./project";
 
 interface ProjectsCarouselProps {
-    projects: { image?: string; title?: string; subtitle?: string; description?: string; link?: string; }[];
+  projects: ProjectProps[];
 }
 
-const ProjectsCarousel = ({
-    projects
-}: ProjectsCarouselProps) => {
-    return (
-        <div className='flex justify-center mt-4'>
-            <Carousel className="w-full max-w-3xl">
-                <CarouselContent className="-ml-2 md:-ml-4">
-                    {projects.map((project, index) => (<CarouselItem key={index} className="basis-1/3 pl-2 md:pl-4"><Project
-                        image={project.image}
-                        title={project.title}
-                        subtitle={project.subtitle}
-                        description={project.description}
-                        link={project.link} /></CarouselItem>))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-                <CarouselDots className="mt-4" />
-            </Carousel>
-        </div>
-    )
+const ProjectsCarousel = ({ projects }: ProjectsCarouselProps) => {
+  return (
+    <div className="flex justify-center mt-4">
+      <Carousel className="w-full max-w-3xl">
+        <CarouselContent className="-ml-2 md:-ml-4">
+          {projects.map((project, index) => (
+            <CarouselItem key={index} className="basis-1/3 pl-2 md:pl-4">
+              <Project
+                image={project.image}
+                title={project.title}
+                subtitle={project.subtitle}
+                description={project.description}
+                link={project.link}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+        <CarouselDots className="mt-4" />
+      </Carousel>
+    </div>
+  );
 };
 
 export default ProjectsCarousel;
-

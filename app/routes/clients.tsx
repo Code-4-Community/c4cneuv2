ffeaf64aee9641df2fe674e15b20255259b5b096
piersolh <145268163+piersolh@users.noreply.github.com>
2024-10-27 16:49:08 -0400

    Client component and list complete

--- app/routes/clients.tsx
+++ app/routes/clients.tsx
@@ -1,4 +1,5 @@
 import { type MetaFunction } from "@remix-run/node";
+import ClientCard from "~/components/client-card";
 import Footer from "~/components/footer";
 
 export const meta: MetaFunction = () => {
@@ -8,6 +9,100 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const exClientsList = [
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+];
+
+const exClientsListPast = [
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+  {
+    image: "app/icons/community.png",
+    title: "Client Title",
+    description: "lorem ipsum",
+    caseStudyLink: "https://www.c4cneu.com",
+    websiteLink: "https://www.c4cneu.com",
+  },
+];
+
 export default function Clients() {
-  return <Footer />;
+  return (
+    <div>
+      <h3 className="text-xl font-semibold text-left mb-2">Current Clients</h3>
+      <div className="flex flex-row">
+        {exClientsList.map((client, index) => (
+          <ClientCard {...client} key={index} />
+        ))}
+      </div>
+      <div>
+        <h3 className="text-xl font-semibold text-left mb-2">Past Clients</h3>
+        <div className="flex flex-row">
+          {exClientsListPast.map((client, index) => (
+            <ClientCard {...client} key={index} />
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }

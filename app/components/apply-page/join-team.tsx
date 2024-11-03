4a40b80caff09f8ea47d94a8e0b6a25d83816cd5
piersolh <145268163+piersolh@users.noreply.github.com>
2024-11-03 14:24:49 -0500

    created apply page and the positions component

---
+++ app/components/apply-page/join-team.tsx
@@ -0,0 +1,24 @@
+const JoinTeamSection = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold  pb-8">
+        Join Our <span className="text-indigo-600"> Team</span>
+      </h1>
+      <h2 className="text-2xl font-semibold text-indigo-600 pb-8"> Members</h2>
+      <h3 className="pb-8">
+        We are a community-driven organization dedicated to creating impactful
+        websites for nonprofits. Our mission is to harness the power of
+        technology to amplify the voices of organizations working towards
+        positive change. By joining us, you'll become part of a collaborative
+        and creative environment where your skills will be put to meaningful
+        use.
+      </h3>
+      <h2 className="text-normal text-indigo-600 pb-8">
+        {" "}
+        We recruit new member prior to the start of each semester. Please learn
+        more about our application process below!
+      </h2>
+    </div>
+  );
+};
+export default JoinTeamSection;

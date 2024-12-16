26551ec6028091cb718c31c965fa13979bafb8fc
David Levin <dinosaurlearning@gmail.com>
2024-12-16 12:06:03 -0500

    add accordion component for faq

--- app/routes/faqs.tsx
+++ app/routes/faqs.tsx
@@ -1,4 +1,10 @@
 import { type MetaFunction } from "@remix-run/node";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../components/ui/accordion";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,6 +13,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// title, description, optional link
+const exFAQ = [
+  { title: "first", description: "first description", link: "apply here" },
+  { title: "second", description: "second description" },
+  { title: "third", description: "third description", link: "clients here" },
+];
+
 export default function FAQs() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold  pb-8">
+        Frequently Asked <span className="text-indigo-600">Questions</span>
+      </h1>
+      {exFAQ.map((item, key) => (
+        <Accordion type="single" collapsible key={key}>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>{item.title}</AccordionTrigger>
+            <AccordionContent>
+              {item.description}
+              {item.link && <p> hi</p>}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      ))}
+    </div>
+  );
 }

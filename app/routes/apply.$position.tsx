3560918e4400a466fa47785291e5c37c6bc37b5a
Max Norman <27299284+maxn990@users.noreply.github.com>
2025-06-06 23:00:10 -0400

    Change title of position pages

--- app/routes/apply.$position.tsx
+++ app/routes/apply.$position.tsx
@@ -6,9 +6,9 @@ import RoleDetailsCard from "~/components/apply-page/role-details";
 import { getPrismicClient } from "~/utils/prismicio";
 import { resolveParams } from "~/utils/util";
 
-export const meta: MetaFunction = () => {
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
   return [
-    { title: "C4C Position" },
+    { title: data?.title ?? "C4C Position" },
     { name: "C4C Position Page", content: "Learn more about our roles" },
   ];
 };
@@ -17,7 +17,14 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   const given_pos = resolveParams(params.position);
   const client = await getPrismicClient();
   const document = await client.getSingle<PositionDocument>("position");
-  return { document, given_pos };
+
+  const position = document.data.position.find(
+    (pos) => asText(pos.name).toLowerCase() === given_pos,
+  );
+
+  const title = position ? asText(position.name) : "C4C Position";
+
+  return { document, given_pos, title };
 };
 
 export default function Role() {
